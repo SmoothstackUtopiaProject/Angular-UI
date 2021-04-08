603c5d863c4928e3c504b3f61c861e343c1b7ed1
SS-christianReyes <christian.reyes@smoothstack.com>
2021-04-07 22:46:21 -0400

    airplane table sorting
    implemented a simple sort for the airplane table

--- src/app/admin-view/airplanes-view/airplanes-view.component.ts
+++ src/app/admin-view/airplanes-view/airplanes-view.component.ts
@@ -1,3 +1,5 @@
+import { AirplanesService } from './../../service/airplanes/airplanes.service';
+import { Airplane } from './../../model/airplane';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirplanesViewComponent implements OnInit {
 
-  constructor() { }
+  airplaneList: Airplane[] = [];
+  currentSort = "up";
+  sortedItem = "";
+
+  constructor(private airplanesService: AirplanesService) { }
 
   ngOnInit(): void {
+    this.getAllAirplanes();
+  }
+
+  onSortChange = (sortedItem : string) => {
+    let nextSort;
+
+    if(this.currentSort === "down") nextSort = "up";
+    else nextSort = "down";
+
+    this.currentSort = nextSort;
+    this.sortedItem = sortedItem;
+    this.sortList();
+  }
+
+  sortList() {
+    let airplaneSorted = this.airplaneList;
+
+    switch(this.sortedItem) {
+      case "airplaneId":
+        airplaneSorted.sort((a,b) => {
+          return this.currentSort === "up"
+            ? a.airplaneId - b.airplaneId
+            : b.airplaneId - a.airplaneId
+        });
+      break;
+
+      case "airplaneTypeId":
+        airplaneSorted.sort((a,b) => {
+          return this.currentSort === "up"
+            ? a.airplaneTypeId - b.airplaneTypeId
+            : b.airplaneTypeId - a.airplaneTypeId
+        });
+      break;
+    }
+
+    this.airplaneList = airplaneSorted;
+  }
+
+  getAllAirplanes(){
+    this.airplanesService.getAllAirplanes().subscribe(
+      response => {
+        this.airplaneList = response;
+        console.log(response)
+        return response;
+      }
+    )
   }
 
 }
